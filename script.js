03a5ab7df33cc4d5ec2b5cb28181a42da89999ce
Claudio Meira <claudiomeirabs@gmail.com>
2023-12-10 20:38:59 -0300

    Requisito 3 e 4 feitos e testados

--- script.js
+++ script.js
@@ -4,3 +4,33 @@ for (let index = 0; index < 25; index += 1) {
   pixels.className = 'pixel';
   pixelBoard.appendChild(pixels);
 }
+
+const selectColor = (eventSource) => {
+  const collors = document.querySelectorAll('.color');
+  // poderia ter sido feito com forEach
+  collors.forEach((color) => {
+    color.classList.remove('selected');
+  });
+  // for (let index = 0; index < collors.length; index += 1) {
+  //   collors[index].classList.remove('selected');
+  // }
+
+  const selected = eventSource.target;
+  selected.classList.add('selected');
+};
+
+const insertCollor = (eventSource) => {
+  const selected = document.querySelector('.color.selected');
+  const cssSelected = window.getComputedStyle(selected, null);
+
+  const SelectedColor = cssSelected.getPropertyValue('background-color');
+
+  const selectedPixel = eventSource.target;
+  selectedPixel.style.backgroundColor = SelectedColor;
+};
+
+const palette = document.querySelector('#color-palette');
+palette.addEventListener('click', selectColor);
+
+const pixel = document.querySelector('#pixel-board');
+pixel.addEventListener('click', insertCollor);
